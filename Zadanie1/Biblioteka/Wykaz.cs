3729ef6410d7115ce1bdc9a9ac7f509204358798
Przemysław Komuda <p.komuda1@gmail.com>
2019-10-16 19:43:50 +0200

    Dodanie klas Wykaz i Katalog

---
+++ Zadanie1/Biblioteka/Wykaz.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Biblioteka
+{
+    public class Wykaz
+    {
+        public string ID { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+
+        public Wykaz()
+        {
+            ID = Guid.NewGuid().ToString();
+            Imie = "Brak imienia";
+            Nazwisko = "Brak nazwiska";
+        }
+
+        public Wykaz(string imie, string nazwisko)
+        {
+            ID = Guid.NewGuid().ToString();
+            Imie = imie;
+            Nazwisko = nazwisko;
+        }
+
+        public Wykaz(string id, string imie, string nazwisko)
+        {
+            ID = id;
+            Imie = imie;
+            Nazwisko = nazwisko;
+        }
+    }
+}
