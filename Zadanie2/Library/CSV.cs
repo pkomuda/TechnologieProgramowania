30cb15905357c73eb7ac50f870c5a71c0ce9b770
Szymon Gruda <szymon_gruda@o2.pl>
2019-11-17 19:27:45 +0100

    Dodanie testów serializacji do csv, bez eventów.

--- Zadanie2/Library/CSV.cs
+++ Zadanie2/Library/CSV.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Library
 {
-    class CSV
+    public class CSV
     {
-        void export(IEnumerable<Client> data, string path2file)
+        public void serialize(IEnumerable<Client> data, string path2file)
         {
             string line = "";
             string catalogs = "";
@@ -18,36 +19,61 @@ namespace Library
                 {
                     catalogs += cat.ID + ';';
                 }
-                line += c.ID + ';' + c.FirstName + ';' + c.LastName + ';' + c.Penalty + ';' + catalogs + "/n";
+                line += c.ID + ';' + c.FirstName + ';' + c.LastName + ';' + c.Penalty + ';' + catalogs + "\n";
                 catalogs = "";
             }
-            System.IO.File.WriteAllText(path2file, line);
+            File.Delete(path2file);
+            using (Stream _stream = File.Open(path2file, FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                byte[] _content = Encoding.ASCII.GetBytes(line);
+                _stream.Write(_content, 0, _content.Length);
+            }
         }
-        void export(IEnumerable<Catalog> data, string path2file)
+        public void serialize(IEnumerable<Catalog> data, string path2file)
         {
             string line = "";
             foreach (Catalog c in data)
             {
-                line += c.ID + ';' + c.Title + ';' + c.Author + "/n";
+                line += c.ID + ';' + c.Title + ';' + c.Author + "\n";
             }
-            System.IO.File.WriteAllText(path2file, line);
+            File.Delete(path2file);
+            using (Stream _stream = File.Open(path2file, FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                byte[] _content = Encoding.ASCII.GetBytes(line);
+                _stream.Write(_content, 0, _content.Length);
+            }
         }
-        void export(IEnumerable<Event> data, string path2file)
+        public void serialize(IEnumerable<Event> data, string path2file)
         {
 
         }
-        void export(IEnumerable<Inventory> data, string path2file)
+        public void serialize(IEnumerable<Inventory> data, string path2file)
         {
             string line = "";
             foreach (Inventory i in data)
             {
-                line += i.Catalog.ID + ';' + i.Amount + "/n";
+                line += i.Catalog.ID + ';' + i.Amount + "\n";
             }
-            System.IO.File.WriteAllText(path2file, line);
+            File.Delete(path2file);
+            using (Stream _stream = File.Open(path2file, FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                byte[] _content = Encoding.ASCII.GetBytes(line);
+                _stream.Write(_content, 0, _content.Length);
+            }
         }
-        void export(IEnumerable<string> data, string path2file)
+        public void serialize(IEnumerable<string> data, string path2file)
         {
-
+            string line = "";
+            foreach (string e in data)
+            {
+                line += e + "\n";
+            }
+            File.Delete(path2file);
+            using (Stream _stream = File.Open(path2file, FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                byte[] _content = Encoding.ASCII.GetBytes(line);
+                _stream.Write(_content, 0, _content.Length);
+            }
         }
     }
 }
---
+++ Zadanie2/LibraryTest/CSVSerializationTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Library;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LibraryTest
+{
+    [TestClass]
+    public class CSVSerializationTest
+    {
+        [TestMethod]
+        public void SerializeClientsTest()
+        {
+            List<Client> clients = new List<Client>();
+            clients.Add(new Client("1", "Jakub", "Nowak"));
+            clients.Add(new Client("2", "Krzesimir", "Mniejszy"));
+            clients.Add(new Client("3", "Kazimierz", "Wielki"));
+
+            CSV csv = new CSV();
+            csv.serialize(clients, "clients.csv");
+            string result = File.ReadAllText("clients.csv");
+            Assert.AreEqual("1;Jakub;Nowak;0;\n2;Krzesimir;Mniejszy;0;\n3;Kazimierz;Wielki;0;\n", result);
+        }
+        [TestMethod]
+        public void SerializeCatalogsTest()
+        {
+            Dictionary<string, Catalog> books = new Dictionary<string, Catalog>();
+            Catalog c1 = new Catalog("1", "Krzyzacy", "Henryk Sienkiewicz");
+            Catalog c2 = new Catalog("2", "Kroniki Czarnej Kompanii", "Glen Cook");
+            Catalog c3 = new Catalog("3", "Pan Tadeusz", "Adam Mickiewicz");
+            books.Add(c1.ID, c1);
+            books.Add(c2.ID, c2);
+            books.Add(c3.ID, c3);
+
+            CSV csv = new CSV();
+            csv.serialize(books.Values, "books.csv");
+            string result = File.ReadAllText("books.csv");
+            //Console.WriteLine(result);
+            Assert.AreEqual("1;Krzyzacy;Henryk Sienkiewicz\n2;Kroniki Czarnej Kompanii;Glen Cook\n3;Pan Tadeusz;Adam Mickiewicz\n", result);
+        }
+        [TestMethod]
+        public void SerializeInventoriesTest()
+        {
+            Dictionary<string, Catalog> books = new Dictionary<string, Catalog>();
+            List<Inventory> inventories = new List<Inventory>();
+            Catalog c1 = new Catalog("1", "Krzyzacy", "Henryk Sienkiewicz");
+            Catalog c2 = new Catalog("2", "Kroniki Czarnej Kompanii", "Glen Cook");
+            Catalog c3 = new Catalog("3", "Pan Tadeusz", "Adam Mickiewicz");
+            books.Add(c1.ID, c1);
+            books.Add(c2.ID, c2);
+            books.Add(c3.ID, c3);
+            inventories.Add(new Inventory(books[c1.ID], 10));
+            inventories.Add(new Inventory(books[c2.ID], 5));
+            inventories.Add(new Inventory(books[c3.ID], 7));
+            CSV csv = new CSV();
+            csv.serialize(inventories, "inventories.csv");
+            string result = File.ReadAllText("inventories.csv");
+            //Console.WriteLine(result);
+            Assert.AreEqual("1;10\n2;5\n3;7\n", result);
+        }
+        [TestMethod]
+        public void SerializeNotificationsTest()
+        {
+            List<string> notifications = new List<string>();
+            notifications.Add("TestNotifications1");
+            notifications.Add("TestNotifications2");
+            notifications.Add("TestNotifications3");
+            CSV csv = new CSV();
+            csv.serialize(notifications, "notifications.csv");
+            string result = File.ReadAllText("notifications.csv");
+            //Console.WriteLine(result);
+            Assert.AreEqual("TestNotifications1\n"+"TestNotifications2\n"+"TestNotifications3\n", result);
+        }
+    }
+}
