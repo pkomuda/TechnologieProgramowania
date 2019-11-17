19f259d6ba8237717317506e8dbf7e526886c6e0
Szymon Gruda <szymon_gruda@o2.pl>
2019-11-17 15:33:15 +0100

    Dodanie interfejsu ISerializeCollection.

---
+++ Zadanie2/Library/ISerializeCollection.cs
@@ -0,0 +1,14 @@
+﻿namespace Library
+{
+    interface ISerializeCollection
+    {
+        void saveClients();
+        void loadClients();
+        void saveCatalogs();
+        void loadCatalogs();
+        void saveInventories();
+        void loadInventories();
+        void saveEvents();
+        void loadEvents();
+    }
+}
