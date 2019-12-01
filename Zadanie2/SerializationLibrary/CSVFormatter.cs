458a8cc311401366fa356bb7c5b70711a61e6b88
Szymon Gruda <szymon_gruda@o2.pl>
2019-12-01 18:35:32 +0100

    Nadpisanie kilku metod "Write...." w CSVFormatter.

--- Zadanie2/SerializationLibrary/CSVFormatter.cs
+++ Zadanie2/SerializationLibrary/CSVFormatter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Runtime.Serialization;
@@ -8,17 +9,17 @@ namespace SerializationLibrary
 {
     class CSVFormatter : Formatter
     {
-        public override SerializationBinder Binder { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override SerializationBinder Binder { get; set; }
         public override StreamingContext Context { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public override ISurrogateSelector SurrogateSelector { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public ObjectIDGenerator IDGenerator { get; set; }
 
-        private string Content = "";
-        private bool FirtsTime;
 
         public CSVFormatter()
         {
             IDGenerator = new ObjectIDGenerator();
+            Binder = new CSVBinder();
+            Context = new StreamingContext(StreamingContextStates.File);
         }
 
         public override object Deserialize(Stream serializationStream)
@@ -26,37 +27,42 @@ namespace SerializationLibrary
             throw new NotImplementedException();
         }
 
+        private string Data = "";
         public override void Serialize(Stream serializationStream, object graph)
         {
             if (graph is ISerializable data)
             {
                 SerializationInfo info = new SerializationInfo(graph.GetType(), new FormatterConverter());
-                info.AddValue("ID", IDGenerator.GetId(graph, out FirtsTime));
-                info.AddValue("Type", graph.GetType().FullName);
-                StreamingContext streamingContext = new StreamingContext(StreamingContextStates.File);
-                data.GetObjectData(info, streamingContext);
-                foreach (SerializationEntry entry in info)
+                Binder.BindToName(graph.GetType(), out string assemblyName, out string typeName);
+                Data += assemblyName + "|" + typeName + "|" + this.IDGenerator.GetId(graph, out bool firstTime);
+                data.GetObjectData(info, Context);
+
+                foreach (SerializationEntry item in info)
                 {
-                    if (entry.Value != null && entry.Value is ISerializable && entry.Value.GetType() != typeof(DateTime))
+                    WriteMember(item.Name, item.Value);
+                }
+                List<string> SaveIT = new List<string>();
+                SaveIT.Add(Data + "\n");
+                Data = "";
+                while (this.m_objectQueue.Count != 0) //Contains a Queue of the objects left to serialize.
+                {
+                    this.Serialize(null, this.m_objectQueue.Dequeue());
+                }
+
+                if (serializationStream != null)
+                {
+                    using (StreamWriter streamWriter = new StreamWriter(serializationStream))
                     {
-                        WriteMember(entry.Name, entry.Value);
-                        if (FirtsTime == true)
+                        foreach (string s in SaveIT)
                         {
-                            Serialize(serializationStream, entry.Value);
-                        }
-                    }
-                    else
-                    {
-                        WriteMember(entry.Name, entry.Value);
+                            streamWriter.Write(s);
+                        }    
                     }
                 }
-                byte[] content = Encoding.UTF8.GetBytes(this.Content);
-                serializationStream.Write(content, 0, content.Length);
-                this.Content = "";
             }
             else
             {
-                throw new ArgumentException("Implementation of is mandatory");
+                throw new ArgumentException("Serialization is not possible. (not ISerializable)");
             }
         }
 
@@ -82,7 +88,7 @@ namespace SerializationLibrary
 
         protected override void WriteDateTime(DateTime val, string name)
         {
-            throw new NotImplementedException();
+            Data += name + "|" + val.ToString("d", DateTimeFormatInfo.InvariantInfo) + "|"; 
         }
 
         protected override void WriteDecimal(decimal val, string name)
@@ -112,7 +118,27 @@ namespace SerializationLibrary
 
         protected override void WriteObjectRef(object obj, string name, Type memberType)
         {
-            throw new NotImplementedException();
+            if (memberType.Equals(typeof(String)))
+            {
+                Data += name + "|" + obj.GetType() + "|" + (string)obj + "|";
+
+            }
+            else
+            {
+                if (null != obj)
+                {
+                    Data += "|" + name + "|" + obj.GetType() + "|" + IDGenerator.GetId(obj, out bool firstTime).ToString();
+                    if (firstTime)
+                    {
+                        this.m_objectQueue.Enqueue(obj);
+                    }
+                }
+                else
+                {
+                    Data += "|" + name + "|null|0";
+                }
+            }
+
         }
 
         protected override void WriteSByte(sbyte val, string name)
@@ -122,7 +148,7 @@ namespace SerializationLibrary
 
         protected override void WriteSingle(float val, string name)
         {
-            this.Content += name + "|" + val.ToString("0.00", CultureInfo.InvariantCulture) + "|";
+            this.Data += name + "|" + val.ToString("0.00", CultureInfo.InvariantCulture) + "|";
         }
 
         protected override void WriteTimeSpan(TimeSpan val, string name)
