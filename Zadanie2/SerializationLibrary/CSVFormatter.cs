94d3e6c334b43c40d25ecaa50126d4d802bee55c
Szymon Gruda <szymon_gruda@o2.pl>
2019-12-04 00:17:55 +0100

    deserializacja i liczne poprawki serializacji

--- Zadanie2/SerializationLibrary/CSVFormatter.cs
+++ Zadanie2/SerializationLibrary/CSVFormatter.cs
@@ -26,7 +26,73 @@ namespace SerializationLibrary
         public override object Deserialize(Stream serializationStream)
         {
             object deserializedObject = null;
+            Dictionary<long, object> deserializedObjects = new Dictionary<long, object>();
+            Dictionary<object, SerializationInfo> objectSerializationInfo = new Dictionary<object, SerializationInfo>();
+            Dictionary<SerializationInfo, List<Tuple<string, Type, long>>> waitingObjects = new Dictionary<SerializationInfo, List<Tuple<string, Type, long>>>();
 
+
+            using (StreamReader streamReader = new StreamReader(serializationStream))
+            {
+                string[] objectValues;
+                Type objectType;
+                SerializationInfo sInfo;
+                object tempObject;
+
+                while (!streamReader.EndOfStream)
+                {
+                    objectValues = streamReader.ReadLine().Split(';');
+                    objectType = Binder.BindToType(objectValues[0], objectValues[1]);
+                    sInfo = new SerializationInfo(objectType, new FormatterConverter());
+                    tempObject = FormatterServices.GetUninitializedObject(objectType);
+
+                    if (deserializedObject == null)
+                        deserializedObject = tempObject;
+
+                    objectSerializationInfo.Add(tempObject, sInfo);
+                    deserializedObjects.Add(long.Parse(objectValues[2]), tempObject);
+                    char[] a = { ';' };
+                    string[] properties = streamReader.ReadLine().Split(a, StringSplitOptions.RemoveEmptyEntries);
+                    foreach (string property in properties)
+                    {
+                        string[] tmp = property.Split('|');
+                        if (tmp[2].StartsWith("ref"))
+                        {
+                            if (!waitingObjects.ContainsKey(sInfo))
+                            {
+                                waitingObjects.Add(sInfo, new List<Tuple<string, Type, long>>());
+                            }
+                            if (tmp[2] == "ref0")
+                            {
+                                sInfo.AddValue(tmp[1], null, Type.GetType(tmp[0]));
+                            }
+                            else
+                            {
+                                waitingObjects[sInfo].Add(new Tuple<string, Type, long>(tmp[1], Type.GetType(tmp[0]), long.Parse(tmp[2].Substring(3))));
+                            }
+                        }
+                        else
+                        {
+                            sInfo.AddValue(tmp[1], Convert.ChangeType(tmp[2], Type.GetType(tmp[0]), CultureInfo.InvariantCulture), Type.GetType(tmp[0]));
+                        }
+                    }
+
+                }
+                foreach (KeyValuePair<SerializationInfo, List<Tuple<string, Type, long>>> keyValuePairWaitingObjects in waitingObjects)
+                {
+                    SerializationInfo serializationInfo = keyValuePairWaitingObjects.Key;
+                    foreach (Tuple<string, Type, long> tuple in keyValuePairWaitingObjects.Value)
+                    {
+                        serializationInfo.AddValue(tuple.Item1, deserializedObjects[tuple.Item3], tuple.Item2);
+                    }
+                }
+
+                foreach (KeyValuePair<object, SerializationInfo> keyValuePair in objectSerializationInfo)
+                {
+                    keyValuePair.Key.GetType().GetConstructor(
+                                                                 new Type[] { typeof(SerializationInfo), typeof(StreamingContext) }
+                                                             ).Invoke(keyValuePair.Key, new object[] { keyValuePair.Value, Context });
+                }
+            }
 
             return deserializedObject;
         }
@@ -140,7 +206,7 @@ namespace SerializationLibrary
                 }
                 else
                 {
-                    Data += name + "|null|ref0";
+                    Data += memberType + "|" + name + "|ref0;";
                 }
             }
         }
@@ -152,7 +218,7 @@ namespace SerializationLibrary
 
         protected override void WriteSingle(float val, string name)
         {
-            this.Data += val.GetType() + "|" + name + "|" + val.ToString("0.00", CultureInfo.InvariantCulture) + ";";
+            this.Data += val.GetType() + "|" + name + "|" + val.ToString(CultureInfo.InvariantCulture) + ";";
         }
 
         protected override void WriteTimeSpan(TimeSpan val, string name)
--- Zadanie2/SerializationLibraryTest/CSVSerializationTest.cs
+++ Zadanie2/SerializationLibraryTest/CSVSerializationTest.cs
@@ -20,9 +20,9 @@ namespace SerializationLibraryTest
             CSVSerialization<A> csv = new CSVSerialization<A>("pliczek.csv", a);
             csv.serialize();
             string result = File.ReadAllText("pliczek.csv");
-            Assert.AreEqual("" + "\n" +
+            Assert.AreEqual(
                 "SerializationLibrary, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null;SerializationLibrary.A;1;" + "\n" +
-                "System.String|Name|A;System.Single|Number|1.10;System.DateTime|Date|12/01/2019;SerializationLibrary.B|ObjectB|ref2;" + "\n" +
+                "System.String|Name|A;System.Single|Number|1.1;System.DateTime|Date|12/01/2019;SerializationLibrary.B|ObjectB|ref2;" + "\n" +
                 "SerializationLibrary, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null;SerializationLibrary.B;2;" + "\n" +
                 "System.String|Name|B;System.Single|Number|3.65;System.DateTime|Date|10/01/2019;SerializationLibrary.C|ObjectC|ref3;" + "\n" +
                 "SerializationLibrary, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null;SerializationLibrary.C;3;" + "\n" +
@@ -31,14 +31,16 @@ namespace SerializationLibraryTest
         [TestMethod]
         public void DeserializeTest()
         {
-            /* A a = new A("A", 1.1f, new DateTime(2019, 12, 1), null);
+             A a = new A("A", 1.1f, new DateTime(2019, 12, 1), null);
              B b = new B("B", 3.65f, new DateTime(2019, 10, 1), null);
              C c = new C("C", 5.37f, new DateTime(2020, 1, 2), null);
              a.ObjectB = b;
              b.ObjectC = c;
              c.ObjectA = a;
+            Console.WriteLine(a.ToString());
              CSVSerialization<A> csv = new CSVSerialization<A>("pliczek.csv", a);
-             A a2 = csv.deserialize();*/
+             A a2 = csv.deserialize();
+            Console.WriteLine(a2.ToString());
 
         }
     }
