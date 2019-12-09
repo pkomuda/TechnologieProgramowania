62f16516b060243786ce7a816f447b6c768d984b
Przemysław Komuda <p.komuda1@gmail.com>
2019-12-10 00:52:45 +0100

    Podstawowe metody rozszerzające i testy do nich

---
+++ Zadanie3/DatabaseLibrary/DatabaseLibrary/LINQExtension.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DatabaseLibrary
+{
+    public static class LINQExtension
+    {
+        public static List<Product> GetProductsWithNoCategoryDeclarative(this List<Product> products)
+        {
+            return (from product in products
+                    where product.ProductSubcategoryID.Equals(null)
+                    select product).ToList();
+        }
+
+        public static List<Product> GetProductsWithNoCategoryImperative(this List<Product> products)
+        {
+            return products.Where(product => product.ProductSubcategoryID.Equals(null)).ToList();
+        }
+
+        public static List<Product> GetNProductsOnPageDeclarative(this List<Product> products, int pageSize, int pageNumber)
+        {
+            return (from product in products
+                    select product).Skip(pageSize * (pageNumber - 1)).Take(pageSize).ToList();
+        }
+
+        public static List<Product> GetNProductsOnPageImperative(this List<Product> products, int pageSize, int pageNumber)
+        {
+            return products.Skip(pageSize * (pageNumber - 1)).Take(pageSize).ToList();
+        }
+    }
+}
---
+++ Zadanie3/DatabaseLibrary/DatabaseLibraryTest/LINQExtensionTest.cs
@@ -0,0 +1,68 @@
+﻿using DatabaseLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+
+namespace DatabaseLibraryTest
+{
+    [TestClass]
+    public class LINQExtensionTest
+    {
+        [TestMethod]
+        public void GetProductsWithNoCategoryDecarativeTest()
+        {
+            using (LINQToSQLDataContext db = new LINQToSQLDataContext())
+            {
+                Table<Product> productTable = db.GetTable<Product>();
+                List<Product> allProducts = (from product in productTable
+                                        select product).ToList();
+                Assert.AreEqual(209, allProducts.GetProductsWithNoCategoryDeclarative().Count);
+            }
+        }
+
+        [TestMethod]
+        public void GetProductsWithNoCategoryImperativeTest()
+        {
+            using (LINQToSQLDataContext db = new LINQToSQLDataContext())
+            {
+                Table<Product> productTable = db.GetTable<Product>();
+                List<Product> allProducts = (from product in productTable
+                                        select product).ToList();
+                Assert.AreEqual(209, allProducts.GetProductsWithNoCategoryImperative().Count);
+            }
+        }
+
+        [TestMethod]
+        public void GetNProductsOnPageDeclarativeTest()
+        {
+            using (LINQToSQLDataContext db = new LINQToSQLDataContext())
+            {
+                Table<Product> productTable = db.GetTable<Product>();
+                List<Product> allProducts = (from product in productTable
+                                     select product).ToList();
+                List<Product> result = allProducts.GetNProductsOnPageDeclarative(3, 2);
+                Assert.AreEqual(3, result.Count);
+                Assert.AreEqual(4, result.ElementAt(0).ProductID);
+                Assert.AreEqual(316, result.ElementAt(1).ProductID);
+                Assert.AreEqual(317, result.ElementAt(2).ProductID);
+            }
+        }
+
+        [TestMethod]
+        public void GetNProductsOnPageImperativeTest()
+        {
+            using (LINQToSQLDataContext db = new LINQToSQLDataContext())
+            {
+                Table<Product> productTable = db.GetTable<Product>();
+                List<Product> allProducts = (from product in productTable
+                                             select product).ToList();
+                List<Product> result = allProducts.GetNProductsOnPageImperative(3, 2);
+                Assert.AreEqual(3, result.Count);
+                Assert.AreEqual(4, result.ElementAt(0).ProductID);
+                Assert.AreEqual(316, result.ElementAt(1).ProductID);
+                Assert.AreEqual(317, result.ElementAt(2).ProductID);
+            }
+        }
+    }
+}
