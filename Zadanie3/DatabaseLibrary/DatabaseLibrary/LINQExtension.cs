94ffba3f48b6db9c055059a48135acfcef18b765
Przemysław Komuda <p.komuda1@gmail.com>
2019-12-10 12:28:03 +0100

    Łączenie nazw produktów i nazw vendorów

--- Zadanie3/DatabaseLibrary/DatabaseLibrary/LINQExtension.cs
+++ Zadanie3/DatabaseLibrary/DatabaseLibrary/LINQExtension.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
 using System.Linq;
 
 namespace DatabaseLibrary
@@ -26,6 +28,46 @@ namespace DatabaseLibrary
         public static List<Product> GetNProductsOnPageImperative(this List<Product> products, int pageSize, int pageNumber)
         {
             return products.Skip(pageSize * (pageNumber - 1)).Take(pageSize).ToList();
+        }
+
+        //skopcone https://stackoverflow.com/questions/22507640/linq-concatenate-values-from-associated-tables
+        public static List<string> GetProductAndVendorNames(this List<Product> products)
+        {
+            using (LINQToSQLDataContext db = new LINQToSQLDataContext())
+            {
+                Table<ProductVendor> pvTable = db.GetTable<ProductVendor>();
+                List<ProductVendor> pvAll = (from productVendor in pvTable
+                                             select productVendor).ToList();
+                Table<Vendor> vTable = db.GetTable<Vendor>();
+                List<Vendor> vAll = (from vendor in vTable
+                                     select vendor).ToList();
+
+                var myQuery = from p in products
+                              join pv in pvAll on p.ProductID equals pv.ProductID
+                              join v in vAll on pv.BusinessEntityID equals v.BusinessEntityID
+                              select new
+                              {
+                                  ProductName = p.Name,
+                                  VendorName = v.Name
+                              };
+
+                List<string> myResult = from q in myQuery.AsEnumerable()
+                                        group q by new { q.ProductName, q.VendorName } into myGroup
+                                        select new InnerProductVendor
+                                        {
+                                            ProductName = myGroup.Key.ProductName,
+                                            VendorName = myGroup.Key.VendorName,
+                                            ProductVendorName = String.Join(",", values: myGroup.Select(q => q.ProductName), myGroup.Select(q => q.VendorName))
+                                        };
+                return myResult;
+            }
+        }
+
+        private class InnerProductVendor
+        {
+            public string ProductName { get; set; }
+            public string VendorName { get; set; }
+            public string ProductVendorName { get; set; }
         }
     }
 }
