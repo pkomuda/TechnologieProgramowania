4bfae24e0c9fa46010f420459c84d4698c162ebf
Przemysław Komuda <p.komuda1@gmail.com>
2020-01-15 21:31:17 +0100

    Struktura projektu, ProductRepository i testy

---
+++ Zadanie4/Test/ProductRepositoryTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model;
+
+namespace Test
+{
+    [TestClass]
+    public class ProductRepositoryTest
+    {
+        private ProductRepository productRepository = new ProductRepository();
+        private Product product = new Product
+        {
+            ProductID = 9876,
+            Name = "TestProductName",
+            ProductNumber = "TestProductNumber",
+            SafetyStockLevel = 1000,
+            ReorderPoint = 750,
+            StandardCost = 0,
+            ListPrice = 0,
+            DaysToManufacture = 0,
+            SellStartDate = DateTime.Now,
+            SellEndDate = DateTime.Now,
+            ModifiedDate = DateTime.Now,
+            rowguid = new Guid()
+        };
+
+        [TestMethod]
+        public void AddProductTest()
+        {
+            int amount = productRepository.GetAllProducts().ToArray().Length;
+            productRepository.AddProduct(product);
+            Assert.AreEqual(amount + 1, productRepository.GetAllProducts().ToArray().Length);
+            productRepository.DeleteProductByName("TestProductName");
+        }
+        
+        [TestMethod]
+        public void GetProductTest()
+        {
+            string productNumber = product.ProductNumber;
+            productRepository.AddProduct(product);
+            Assert.AreEqual(productNumber, productRepository.GetProductByName("TestProductName").ProductNumber);
+            productRepository.DeleteProductByName("TestProductName");
+        }
+        
+        [TestMethod]
+        public void GetAllProductsTest()
+        {
+            ProductRepository productRepository = new ProductRepository();
+            Assert.IsTrue(productRepository.GetAllProducts().ToArray().Length > 1);
+            Assert.IsNotNull(productRepository.GetAllProducts().ToArray()[0].ProductID);
+            Assert.IsNotNull(productRepository.GetAllProducts().ToArray()[1].ProductID);
+        }
+        
+        [TestMethod]
+        public void UpdateProductTest()
+        {
+            productRepository.AddProduct(product);
+            product.ProductNumber = "NewProductNumber";
+            productRepository.UpdateProductByName("TestProductName", product);
+            Assert.AreEqual("NewProductNumber", productRepository.GetProductByName("TestProductName").ProductNumber);
+            productRepository.DeleteProductByName("TestProductName");
+        }
+        
+        [TestMethod]
+        public void DeleteProductTest()
+        {
+            productRepository.AddProduct(product);
+            int amount = productRepository.GetAllProducts().ToArray().Length;
+            productRepository.DeleteProductByName("TestProductName");
+            Assert.AreEqual(amount - 1, productRepository.GetAllProducts().ToArray().Length);
+        }
+    }
+}
---
+++ Zadanie4/Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9cce8a5c-8f7d-4a0b-905a-f1c8ec7163a4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
