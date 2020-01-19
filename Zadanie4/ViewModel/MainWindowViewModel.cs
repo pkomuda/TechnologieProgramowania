4de22c937a43e925c2ebd7304c1a911226425e92
Przemysław Komuda <p.komuda1@gmail.com>
2020-01-19 20:32:07 +0100

    List of product IDs and names in view

---
+++ Zadanie4/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Data;
+using Service;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ViewModel
+{
+    public class MainWindowViewModel
+    {
+        private ProductRepository productRepository = new ProductRepository();
+        public List<Product> Products
+        {
+            get
+            {
+                return productRepository.GetAllProducts().ToList();
+            }
+        }
+    }
+}
