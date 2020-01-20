34d53774f462c23deeaef51db62447ce0d2f0747
Szymon Gruda <szymon_gruda@o2.pl>
2020-01-20 21:01:47 +0100

    RelayCommand and ViewModelBase

--- Zadanie4/ViewModel/MainWindowViewModel.cs
+++ Zadanie4/ViewModel/MainWindowViewModel.cs
@@ -1,19 +1,80 @@
 ﻿using Data;
 using Service;
 using System.Collections.Generic;
-using System.Linq;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 
 namespace ViewModel
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : ViewModelBase
     {
-        private ProductRepository productRepository = new ProductRepository();
-        public List<Product> Products
+        //private ProductRepository ProductRepository = new ProductRepository();
+         /*public List<Product> Products
+         {
+             get
+             {
+                 return ProductRepository.GetAllProducts().ToList();
+             }
+         }*/
+        public MainWindowViewModel()
+        {
+            AddProductCommand = new RelayCommand(AddProduct);
+        }
+        private Product m_Product;
+        public Product Product
         {
             get
             {
-                return productRepository.GetAllProducts().ToList();
+                return m_Product;
             }
+            set
+            {
+                m_Product = value;
+                //RaisePropertyChanged();
+            }
+        }
+        private ObservableCollection<Product> m_Products;
+        public ObservableCollection<Product> Products
+        {
+            get { return m_Products; }
+            set
+            {
+                m_Products = value;
+                //RaisePropertyChanged();
+            }
+        }
+        private ProductRepository m_ProductRepository;
+        public ProductRepository ProductRepository
+        {
+            get { return m_ProductRepository; }
+            set
+            {
+                m_ProductRepository = value;
+
+                Task.Run(() =>
+                {
+                    Products = new ObservableCollection<Product>(value.GetAllProducts());
+                });
+            }
+        }
+        public string Name { get; set; }
+        public void AddProduct()
+        {
+            Product product = new Product
+            {
+                Name = Name,
+            };
+
+            Task.Run(() =>
+            {
+                m_ProductRepository.AddProduct(product);
+
+            });
+
+        }
+        public RelayCommand AddProductCommand
+        {
+            get; private set;
         }
     }
 }
