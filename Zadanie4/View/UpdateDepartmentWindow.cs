c89464b7e85117b7128d72bb654bbc1d4fe923ac
Szymon Gruda <szymon_gruda@o2.pl>
2020-01-21 20:20:15 +0100

    Add DepartmentView

--- Zadanie4/View/UpdateDepartmentWindow.cs
+++ Zadanie4/View/UpdateDepartmentWindow.cs
@@ -5,10 +5,10 @@ namespace View
     class UpdateDepartmentWindow : IWindow
     {
         public event VoidHandler OnClose;
-        private UpdateDepartmentWindow _window;
+        private DepartmentView _window;
         public UpdateDepartmentWindow()
         {
-            _window = new UpdateDepartmentWindow();
+            _window = new DepartmentView();
         }
         public void BindViewModel<T>(T viewModel) where T : ViewModelBase
         {
