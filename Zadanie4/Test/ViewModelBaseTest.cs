e5073e5ba131bbcc4883d159cd03ea4cdaebeaab
Szymon Gruda <szymon_gruda@o2.pl>
2020-01-22 21:20:55 +0100

    ViewModelBaseTest

---
+++ Zadanie4/Test/ViewModelBaseTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.ComponentModel;
+using ViewModel;
+
+namespace Test
+{
+    [TestClass]
+    public class ViewModelBaseTest
+    {
+        [TestMethod]
+        public void ViewModelBaseInterfaceTest()
+        {
+            ViewModelBaseToTest _toTest = new ViewModelBaseToTest();
+            int _PropertyChangedCount = 0;
+            string _lastPropertyName = String.Empty;
+            _toTest.PropertyChanged += (object sender, PropertyChangedEventArgs e) => { _PropertyChangedCount++; _lastPropertyName = e.PropertyName; };
+            _toTest.TestRaisePropertyChanged("Name");
+            Assert.AreEqual<string>("Name", _lastPropertyName);
+            Assert.AreEqual<int>(1, _PropertyChangedCount);
+        }
+        private class ViewModelBaseToTest : ViewModelBase
+        {
+            internal void TestRaisePropertyChanged(string propertyName)
+            {
+                base.RaisePropertyChanged(propertyName);
+            }
+        }
+    }
+}
